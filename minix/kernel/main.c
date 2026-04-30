d5856abaf67823338578831d3f0df2e102994410
nicolascabanas <cabanas@unifesp.br>
2026-04-30 00:06:20 +0000

    Adicionando banners da Equipe 7 no exec.c

--- minix/kernel/main.c
+++ minix/kernel/main.c
@@ -342,7 +342,13 @@ static void announce(void)
 #endif
       "Copyright 2016, Vrije Universiteit, Amsterdam, The Netherlands\n",
       OS_RELEASE);
-  printf("MINIX is open source software, see http://www.minix3.org\n");
+printf("\n";
+printf("                ========================================\n");
+printf("                | Minix 3.4.0rc6 - SO - UNIFESP 1s2026 |\n");
+printf("                |    Projeto Pratico (I) - Equipe 7    |\n");
+printf("                ========================================\n");
+  printf(\n"MINIX is open source software, see 
+http://www.minix3.org\n");
 }
 
 /*===========================================================================*
@@ -358,6 +364,12 @@ void prepare_shutdown(const int how)
    * argument passes the shutdown status. 
    */
   printf("MINIX will now be shut down ...\n");
+printf("\n);
+printf("                 =======================================\n");
+printf("                 | ATE A PROXIMA - SO - UNIFESP 1s2026 |\n");
+printf("                 |    Projeto Pratico (I) - Equipe 7   |\n");
+printf("                 =======================================\n");
+printf("\n");
   set_kernel_timer(&shutdown_timer, get_monotonic() + system_hz,
       minix_shutdown, how);
 }
