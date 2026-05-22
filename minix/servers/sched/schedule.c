1c6305faabd3eed5c17742a9e7807597a7b04129
Yuji Furukawa <165024160+yujifkw@users.noreply.github.com>
2026-05-22 14:53:08 -0300

    Lucas Yuji: Removido a chamada de balanceamento periódico

--- minix/servers/sched/schedule.c
+++ minix/servers/sched/schedule.c
@@ -328,16 +328,17 @@ static int schedule_process(struct schedproc * rmp, unsigned flags)
 
 
 /*===========================================================================*
- *				init_scheduling				     *
+ *				init_scheduling (MODIFICADO)			     *
  *===========================================================================*/
 void init_scheduling(void)
 {
-	int r;
+	/* * REMOVIDO: A chamada para sys_setalarm que agendava a execucao
+	 * periodica de balance_queues() foi removida.
+	 */
+	// balance_timeout = BALANCE_TIMEOUT * sys_hz();
 
-	balance_timeout = BALANCE_TIMEOUT * sys_hz();
-
-	if ((r = sys_setalarm(balance_timeout, 0)) != OK)
-		panic("sys_setalarm failed: %d", r);
+	// if ((r = sys_setalarm(balance_timeout, 0)) != OK)
+	// 	panic("sys_setalarm failed: %d", r);
 }
 
 /*===========================================================================*
